fb83a60cccfe73e7e5bf86760379b83a9135e598
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-10-05 20:37:23 -0400

    committing current WIP, have the findbyvalue almost working but working through the last for loop

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -58,6 +58,28 @@ namespace TechJobsConsole
             return jobs;
         }
 
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                for (int i = 0; i < row.Count; i++)
+                {
+                    string aValue = row[i].ToLower();
+
+                    if (aValue.Contains(value))
+                    {
+                        jobs.Add(row);
+                        break;
+                    }
+                }
+                return jobs;
+            }
+        }
+
         /*
          * Load and parse data from job_data.csv
          */
